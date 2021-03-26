b20450c0f07280c965e61a5c6d3b99ad13dde583
Rodrigo Santos <rodrigosantos9000@gmail.com>
2021-03-26 12:27:09 +0000

    MongoError configuration

--- utils/database.ts
+++ utils/database.ts
@@ -5,12 +5,16 @@ type ConnectType = {
   client: MongoClient
 }
 
+type ErrorType = {
+  message: MongoError
+}
+
 const client = new MongoClient(process.env.DATABASE_URL!, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
-export default async function connect(): Promise<ConnectType> {
+export default async function connect(): Promise<ConnectType | ErrorType> {
   try {
     if(!client.isConnected()) await client.connect()
     const db = client.db('your-mongo-database-here')
@@ -19,6 +23,6 @@ export default async function connect(): Promise<ConnectType> {
       client
     }   
   } catch (error) {
-    throw new MongoError('Server temporarily down')
+    throw new MongoError({ message: 'Server temporarily unavailable' })
   }
 }
